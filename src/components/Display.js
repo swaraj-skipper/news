ae7a7adbe49a977163d41c6fa4d6e227e3863410
swaraj-skipper <76423625+swaraj-skipper@users.noreply.github.com>
2022-08-15 02:31:56 +0530

    basic logic done for maintaining cricket score

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,17 +5,19 @@ import { useState } from 'react'
 
 export default function Display() {
   const [run, setRun] = useState(0);
+  const [currentRun, setCurrentRun ] = useState(0);
   const [wicket, setWicket] = useState(0);
   const arr = [1, 2, 3,4,5,6];
   const [over, setOver] = useState(0);
   const [ball, setBall] = useState(0);
 
   const runCount = (x) => {
+    setCurrentRun(x);
     setRun(run + x);
     setFreeHit(false);
     ballInc();
   }
-
+// function that checks if increased run is 3 and run out is true then setIncreased run = 2
   const freeRun = (x) => {
     setRun(run + 1);
     if (x) {
@@ -26,6 +28,9 @@ export default function Display() {
   const fallWicket = () => {
     setWicket(wicket + 1);
     ballInc();
+    if (wicket === 9) {
+      setBlock(true);
+    }
   }
 
   const ballInc = () =>{
@@ -35,6 +40,9 @@ export default function Display() {
       setOver(over + 1);
       setBall(0);
     }
+    if (over === 9 && ball === 5) {
+      setBlock(true);
+    }
   }
 
   const resetAll = () => {
@@ -43,6 +51,7 @@ export default function Display() {
     setOver(0);
     setBall(0);
     setFreeHit(false);
+    setBlock(false);
   }
 
   const handleBall = () => {
@@ -72,8 +81,10 @@ export default function Display() {
     }
   }
 
+
   const [visible, setVisible] = useState(false)
   const [freeHit, setFreeHit] = useState(false)
+  const [block, setBlock] = useState(false)
 
   // const handleWide = () => {
   //   if(visible)
@@ -90,12 +101,12 @@ export default function Display() {
       <div className="container board">
 
         <div className="container coli">
-          <button className='btn' onClick={() => runCount(1)}>1 Run</button>
-          <button className='btn' onClick={() => runCount(2)}>2 Runs</button>
-          <button className='btn' onClick={() => runCount(3)}>3 Runs</button>
-          <button className='btn' onClick={() => runCount(4)}>4 Runs</button>
-          <button className='btn' onClick={() => runCount(5)}>5 Runs</button>
-          <button className='btn' onClick={() => runCount(6)}>6 Runs</button>
+          <button className='btn' disabled={block} onClick={() => runCount(1)}>1 Run</button>
+          <button className='btn' disabled={block} onClick={() => runCount(2)}>2 Runs</button>
+          <button className='btn' disabled={block} onClick={() => runCount(3)}>3 Runs</button>
+          <button className='btn' disabled={block} onClick={() => runCount(4)}>4 Runs</button>
+          <button className='btn' disabled={block} onClick={() => runCount(5)}>5 Runs</button>
+          <button className='btn' disabled={block} onClick={() => runCount(6)}>6 Runs</button>
         </div>
 
         <div className='container'>
@@ -104,20 +115,20 @@ export default function Display() {
         </div>
 
         <div className="container coli">
-          <button className='btn' disabled={freeHit} onClick={fallWicket}>Out</button>
-          <button className='btn' onClick={ballInc}>Dot</button>
+          <button className='btn' disabled={freeHit || block} onClick={fallWicket}>Out</button>
+          <button className='btn' disabled={block} onClick={ballInc}>Dot</button>
 
-          <button className='btn2' disabled={!freeHit} onClick={fallWicket}>Run-Out</button>
+          <button className='btn2' disabled={!freeHit || block} onClick={fallWicket}>Run-Out</button>
 
-          <button className='btn' onClick={() => freeRun(0)}>Wide</button>
-          <button className='btn' onClick={() => freeRun(1)}>No-Ball</button>
+          <button className='btn' disabled={block} onClick={() => freeRun(0)}>Wide</button>
+          <button className='btn' disabled={block} onClick={() => freeRun(1)}>No-Ball</button>
         </div>
       </div>
       <div className="extras">
         
         <div className='container'>
-          <button className='btn' onClick={() => handleFreeRun(0)}>Wide(bye)</button>
-          <button className='btn' onClick={() => handleFreeRun(1)}>No-Ball(bye)</button>
+          <button className='btn' disabled={block} onClick={() => handleFreeRun(0)}>Wide(bye)</button>
+          <button className='btn' disabled={block} onClick={() => handleFreeRun(1)}>No-Ball(bye)</button>
 
           {visible && <div>
             <button className='btn' onClick={() => handleRun(1)}>1 Run</button>
@@ -131,11 +142,12 @@ export default function Display() {
       </div>
         </div>
       <div className="container">
-        <button className='btn butt' onClick={() => setRun(run-1)}>Decrease Run</button>
+        <button className='btn butt' disabled={block || !run} onClick={() => setRun(run-1)}>Decrease Run</button>
+        <button className='btn butt' disabled={block || (!ball && !over)} onClick={handleBall}>Decrease Ball</button>
+        <button className='btn butt' disabled={!wicket || block} onClick={() => setWicket(wicket-1)}>Decrease Wicket</button>
         <button className='btn butt' onClick={resetAll}>Reset Score</button>
-        <button className='btn butt' onClick={handleBall}>Decrease Ball</button>
       </div>
-
+      <iframe src="https://giphy.com/embed/ubPy6WbMp9dG65qnSd" style={{ width: "480", height: "269", frameBorder: "0" }} className="giphy-embed" ></iframe>
     </>
   )
 }
